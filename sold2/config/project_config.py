0501c058fd774adcff5fbc3e7ec38edfc7e1ca1a
sublatelog <blueganz@gmail.com>
2022-05-18 16:54:39 +0900

    Update project_config.py

--- sold2/config/project_config.py
+++ sold2/config/project_config.py
@@ -9,7 +9,7 @@ class Config(object):
     #####################
     ## Dataset setting ##
     #####################
-    DATASET_ROOT = os.getenv("DATASET_ROOT", "/content/drive/MyDrive/kaggle/ImageMatchingChallenge2022/wireframe")  # TODO: path to your datasets folder
+    DATASET_ROOT = os.getenv("DATASET_ROOT", "/content/drive/MyDrive/kaggle/ImageMatchingChallenge2022/wireframe/")  # TODO: path to your datasets folder
 #     DATASET_ROOT = os.getenv("DATASET_ROOT", "./datasets/")  # TODO: path to your datasets folder
     if not os.path.exists(DATASET_ROOT):
         os.makedirs(DATASET_ROOT)
